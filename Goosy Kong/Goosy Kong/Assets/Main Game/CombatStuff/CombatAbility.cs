f35e90b551f55a916975a951cd63977f8b128cd5
George Williams <32927590+CyberGW@users.noreply.github.com>
2018-02-15 20:57:39 +0000

    NOTE: NOW WORKING FROM WITHIN GOOSY KONG PROJECT Merged main game and Goosy Kong into one project (base Goosy Kong)

---
+++ Goosy Kong/Goosy Kong/Assets/Main Game/CombatStuff/CombatAbility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public interface CombatAbility {
+	int minTargets {get;}
+	int maxTargets {get;}
+	int energyCost {get;}
+	bool isAssist {get;}
+	string abilityName {get;}
+
+	void doAbility (List<CombatCharacter> targets, CombatCharacter user);
+
+}
