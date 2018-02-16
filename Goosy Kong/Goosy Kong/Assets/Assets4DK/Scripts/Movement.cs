5aeb46aaac471a4c20f26a33349773b49a5ccdd9
George Williams <32927590+CyberGW@users.noreply.github.com>
2018-02-16 12:00:57 +0000

    Applied Marius' Goosy Kong update

---
+++ Goosy Kong/Goosy Kong/Assets/Assets4DK/Scripts/Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	public float moveSpeed;
+	public float jumpHeight;
+
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	bool grounded = true;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate() {
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space) && grounded) {
+			Jump ();
+
+		}
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+
+		}
+		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+
+		}
+
+	}
+
+	public void Jump(){
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpHeight);
+
+	}
+}
+
