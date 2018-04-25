78061fdaeb0c8076bcad79a54d17d4e298f5a949
hell2o <jes566@york.ac.uk>
2018-04-25 22:04:58 +0100

    added unit test to check that quests progress correctly

---
+++ Unusual Geese/Assets/Main Game/Unit tests/QuestTests.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class QuestTests {
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator QuestsProgress() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+		SceneManager.LoadScene ("Main Game/Scenes/RonCookeHub");
+		yield return null;
+
+		GameStateManager state = GameStateManager.getGameStateManager ();
+		state.state.s1Quest = QuestData.Stage1QuestTypes.GoosyKongScore1;
+		state.state.s2Quest = QuestData.Stage2QuestTypes.GoosyKongScore2;
+		state.state.s3Quest = QuestData.Stage3QuestTypes.GoosyKongScore3;
+		state.state.questState = 0;
+
+		state.setGameVar ("GoosyKongHighScore", "0");
+		state.tryQuestProgression ();
+		Assert.AreEqual (state.state.questState, 0);
+		state.setGameVar ("GoosyKongHighScore", "1");
+		state.tryQuestProgression ();
+		Assert.AreEqual (state.state.questState, 1);
+		state.setGameVar ("GoosyKongHighScore", "2");
+		state.tryQuestProgression ();
+		Assert.AreEqual (state.state.questState, 2);
+		state.setGameVar ("GoosyKongHighScore", "3");
+		state.tryQuestProgression ();
+		Assert.AreEqual (state.state.questState, 3);
+
+	}
+}
